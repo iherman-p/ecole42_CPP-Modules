f5ec165a79a404c53e169a41d9c611706bebef03
Igor P <200969603+iherman-p@users.noreply.github.com>
2026-01-31 15:17:23 +0100

    01.18 - uhhh

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -6,20 +6,31 @@
 /*   By: iherman- <iherman-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 17:28:49 by iherman-          #+#    #+#             */
-/*   Updated: 2026/01/18 17:43:11 by iherman-         ###   ########.fr       */
+/*   Updated: 2026/01/31 15:05:08 by iherman-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
 
-int	main()
+int main()
 {
-	int	a = 1;
-	int	b = 2;
-	int	c = 3;
+	int a = 2;
+	int b = 3;
 
-	swap(a, b);
-	std::cout << min(a, b) << std::endl;
-	std::cout << max(a, c) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap( a, b );
+	std::cout << "after swap: a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	
+	std::string c = "string1";
+	std::string d = "string2";
+	
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	swap(c, d);
+	std::cout << "after swap: c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	return 0;
 }
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iherman- <iherman-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 18:01:51 by iherman-          #+#    #+#             */
-/*   Updated: 2026/01/18 17:40:41 by iherman-         ###   ########.fr       */
+/*   Updated: 2026/01/31 15:08:19 by iherman-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,31 @@
 
 int	main()
 {
-	const int	array_size = 5;
-	int			array_a[array_size] = {0, 0, 0, 0, 0};
-	char		array_b[array_size] = {'A', 'A', 'A', 'A', 'A'};
+	const int	kArraySize = 5;
+	int			array_a[kArraySize] = {0, 0, 0, 0, 0};
+	char		array_b[kArraySize] = {'A', 'A', 'A', 'A', 'A'};
 
 	std::cout << "test 1:" << std::endl;
 
-	print_arr(array_a, array_size);
+	print_arr(array_a, kArraySize);
 	std::cout << std::endl;
-	print_arr(array_b, array_size);
+	print_arr(array_b, kArraySize);
 	std::cout << "\nafter increment func: " << std::endl;
 
-	iter(array_a, array_size, increment_element);
-	iter(array_b, array_size, increment_element);
+	iter(array_a, kArraySize, increment_element);
+	iter(array_b, kArraySize, increment_element);
 
-	print_arr(array_a, array_size);
+	print_arr(array_a, kArraySize);
 	std::cout << std::endl;
-	print_arr(array_b, array_size);
+	print_arr(array_b, kArraySize);
+	std::cout << std::endl;
+
+	std::cout << "\nafter incrementing 3 elements: " << std::endl;
+	iter(array_a, 3, increment_element);
+	iter(array_b, 3, increment_element);
+
+	print_arr(array_a, kArraySize);
+	std::cout << std::endl;
+	print_arr(array_b, kArraySize);
 	std::cout << std::endl;
 }
